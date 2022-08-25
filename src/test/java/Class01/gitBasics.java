c60406c6c9164d548c9ab48e84f81fd938c5156a
Rachid <106281937+E-Rachid@users.noreply.github.com>
2022-08-24 20:57:44 -0400

    Temp Checker01

--- src/test/java/Class01/gitBasics.java
+++ src/test/java/Class01/gitBasics.java
@@ -3,6 +3,11 @@ package Class01;
 public class gitBasics {
     public static void main(String[] args) {
         // write down a code that check temp from a website and then prints out that  is beautiful day or not
-        System.out.println("Today is Beautiful Day");
-    }
+        System.out.println("Today is not not Beautiful Day");
+
+        int i;
+        for (i=1 ; i>10; i++){
+        System.out.println(i);
+    }}
+
 }
